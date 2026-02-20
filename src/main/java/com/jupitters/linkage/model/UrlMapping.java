0cd8e463f5ff1b67712904ff08347a628de6e64a
jupitters <sampaiobale@gmail.com>
2026-02-20 20:48:04 -0300

    user model

--- src/main/java/com/jupitters/linkage/model/UrlMapping.java
+++ src/main/java/com/jupitters/linkage/model/UrlMapping.java
@@ -1,8 +1,18 @@
 package com.jupitters.linkage.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Data
 public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String originalUrl;
     private String shortUrl;
