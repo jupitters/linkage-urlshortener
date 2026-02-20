63919002b2a76e080e5f08ea5828b561e03a4a3f
jupitters <sampaiobale@gmail.com>
2026-02-20 20:55:47 -0300

    click event model

--- src/main/java/com/jupitters/linkage/model/ClickEvent.java
+++ src/main/java/com/jupitters/linkage/model/ClickEvent.java
@@ -1,9 +1,6 @@
 package com.jupitters.linkage.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -15,5 +12,8 @@ public class ClickEvent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime clickDate;
+
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
     private UrlMapping urlMapping;
 }
