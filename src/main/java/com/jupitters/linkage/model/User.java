d73f31224852c0a97b4ee806a703005d90bf20b6
jupitters <sampaiobale@gmail.com>
2026-02-20 20:40:16 -0300

    user model

---
+++ src/main/java/com/jupitters/linkage/model/User.java
@@ -0,0 +1,9 @@
+package com.jupitters.linkage.model;
+
+public class User {
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String role = "ROLE_USER";
+}
