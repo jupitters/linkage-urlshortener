44cac350b4f7546f39a35f4f2049d2270d8a35fb
jupitters <sampaiobale@gmail.com>
2026-02-20 21:29:39 -0300

    jwt utils

--- src/main/java/com/jupitters/linkage/security/jwt/JwtUtils.java
+++ src/main/java/com/jupitters/linkage/security/jwt/JwtUtils.java
@@ -1,4 +1,9 @@
 package com.jupitters.linkage.security.jwt;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class JwtUtils {
+    public String getJwtFromHeader(HttpServletRequest request){
+        String token = request.getHeader("Authorization");
+    }
 }
