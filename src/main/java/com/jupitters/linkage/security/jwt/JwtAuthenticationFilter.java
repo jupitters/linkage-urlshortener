7e5c5c72d5b47b90f0b3667d5508e3c6e2fa56b5
jupitters <sampaiobale@gmail.com>
2026-02-21 16:17:26 -0300

    authentication filter

---
+++ src/main/java/com/jupitters/linkage/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.jupitters.linkage.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
