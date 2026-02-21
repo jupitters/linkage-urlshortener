ed239824de77e39071a286c291bcdf05b3246e8a
jupitters <sampaiobale@gmail.com>
2026-02-21 15:44:06 -0300

    jwt utils

--- src/main/java/com/jupitters/linkage/security/jwt/JwtUtils.java
+++ src/main/java/com/jupitters/linkage/security/jwt/JwtUtils.java
@@ -5,5 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 public class JwtUtils {
     public String getJwtFromHeader(HttpServletRequest request){
         String token = request.getHeader("Authorization");
+        if(token != null && token.startsWith("Bearer")){
+            return token.substring(7);
+        }
+        return null;
     }
 }
