fd1dc709295839a45e2c5c58394a7fb4d532dbb6
jupitters <sampaiobale@gmail.com>
2026-02-21 16:49:34 -0300

    user repository

--- src/main/java/com/jupitters/linkage/service/UserDetailsServiceImpl.java
+++ src/main/java/com/jupitters/linkage/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jupitters.linkage.service;
 
 import com.jupitters.linkage.model.User;
+import com.jupitters.linkage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
