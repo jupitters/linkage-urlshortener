22869ac4eb4e3aab3f4b94d99d5c3e822e527d59
jupitters <sampaiobale@gmail.com>
2026-02-21 15:52:57 -0300

    jwt utils

--- src/main/java/com/jupitters/linkage/security/jwt/JwtUtils.java
+++ src/main/java/com/jupitters/linkage/security/jwt/JwtUtils.java
@@ -1,6 +1,11 @@
 package com.jupitters.linkage.security.jwt;
 
+import com.jupitters.linkage.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JwtUtils {
     public String getJwtFromHeader(HttpServletRequest request){
@@ -9,5 +14,19 @@ public class JwtUtils {
             return token.substring(7);
         }
         return null;
+    }
+    
+    public String generateToken(UserDetailsImpl user){
+        String username = user.getUsername();
+        String roles = user.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .collect(Collectors.joining(","));
+        return Jwts.builder()
+                .subject(username)
+                .claim("roles", roles)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + 172800000))
+                .signWith()
+                .compact();
     }
 }
