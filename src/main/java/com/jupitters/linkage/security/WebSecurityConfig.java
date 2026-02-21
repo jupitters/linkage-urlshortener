0a1e80cf398670c152d02f23e5a480464cd29b6f
jupitters <sampaiobale@gmail.com>
2026-02-21 17:23:27 -0300

    web security config

--- src/main/java/com/jupitters/linkage/security/WebSecurityConfig.java
+++ src/main/java/com/jupitters/linkage/security/WebSecurityConfig.java
@@ -1,12 +1,32 @@
 package com.jupitters.linkage.security;
 
+import com.jupitters.linkage.security.jwt.JwtAuthenticationFilter;
+import com.jupitters.linkage.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/urls/**").authenticated()
+                        .requestMatchers("/{shortUrl}").permitAll()
+                        .anyRequest().authenticated());
 
+        return http.build();
+    }
 }
