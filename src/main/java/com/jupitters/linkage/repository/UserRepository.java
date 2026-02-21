c6e81f58962883f4d2db646ce95c20a2abae69c7
jupitters <sampaiobale@gmail.com>
2026-02-21 16:44:20 -0300

    user repository

---
+++ src/main/java/com/jupitters/linkage/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jupitters.linkage.repository;
+
+import com.jupitters.linkage.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
--- src/main/java/com/jupitters/linkage/service/UserDetailsServiceImpl.java
+++ src/main/java/com/jupitters/linkage/service/UserDetailsServiceImpl.java
@@ -1,12 +1,20 @@
 package com.jupitters.linkage.service;
 
+import com.jupitters.linkage.model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
         return null;
     }
 }
