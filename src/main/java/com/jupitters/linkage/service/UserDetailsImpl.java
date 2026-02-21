1c1a1d4da57d33ac18994f692911ef2e491f7d9c
jupitters <sampaiobale@gmail.com>
2026-02-20 21:20:31 -0300

    user details

---
+++ src/main/java/com/jupitters/linkage/service/UserDetailsImpl.java
@@ -0,0 +1,36 @@
+package com.jupitters.linkage.service;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
