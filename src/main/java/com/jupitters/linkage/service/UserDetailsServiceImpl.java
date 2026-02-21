db2cba8e618d23ab74ed57e5f7403f0a646acac5
jupitters <sampaiobale@gmail.com>
2026-02-21 16:40:24 -0300

    user details service

---
+++ src/main/java/com/jupitters/linkage/service/UserDetailsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jupitters.linkage.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
