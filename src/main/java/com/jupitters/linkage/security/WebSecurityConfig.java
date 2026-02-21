7496f13be3bdd824f00dceae7651e7f8bb5b0814
jupitters <sampaiobale@gmail.com>
2026-02-21 17:25:54 -0300

    web security config

--- src/main/java/com/jupitters/linkage/security/WebSecurityConfig.java
+++ src/main/java/com/jupitters/linkage/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/{shortUrl}").permitAll()
                         .anyRequest().authenticated());
 
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
