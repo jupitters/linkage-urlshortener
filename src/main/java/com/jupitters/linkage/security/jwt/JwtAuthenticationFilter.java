a03c4664576346a9ef2ea301a47390adcd6c046f
jupitters <sampaiobale@gmail.com>
2026-02-21 16:27:49 -0300

    authentication filter

--- src/main/java/com/jupitters/linkage/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/jupitters/linkage/security/jwt/JwtAuthenticationFilter.java
@@ -5,18 +5,37 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtils jwtUtils;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try{
-
+            String jwt = jwtUtils.getJwtFromHeader(request);
+            if(jwt != null && jwtUtils.validateToken(jwt)){
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+                UserDetails user = userDetailsService.loadUserByUsername(username);
+                if(user != null){
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
