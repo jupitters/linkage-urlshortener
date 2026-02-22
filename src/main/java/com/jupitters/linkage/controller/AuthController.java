1534419ccd7866cd0bc4c1ce9de507601b675421
jupitters <sampaiobale@gmail.com>
2026-02-22 13:58:42 -0300

    auth controller

--- src/main/java/com/jupitters/linkage/controller/AuthController.java
+++ src/main/java/com/jupitters/linkage/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.jupitters.linkage.controller;
 
+import com.jupitters.linkage.dto.RegisterRequest;
+import com.jupitters.linkage.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(registerRequest.getPassword());
+        user.setRole("USER_ROLE");
+        userService.registerUser(user);
 
+        return ResponseEntity.ok("User registered successfully");
     }
 }
