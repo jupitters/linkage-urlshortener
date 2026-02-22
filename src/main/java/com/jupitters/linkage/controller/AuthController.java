6c7d1a01f040445e7ed63eb5ee6a3d5e7de10e01
jupitters <sampaiobale@gmail.com>
2026-02-22 13:49:48 -0300

    auth controller

--- src/main/java/com/jupitters/linkage/controller/AuthController.java
+++ src/main/java/com/jupitters/linkage/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.jupitters.linkage.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest){
+
+    }
 }
