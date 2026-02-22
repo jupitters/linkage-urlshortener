bf9f2dba382b28be979449b4da2fac6fd1addf8f
jupitters <sampaiobale@gmail.com>
2026-02-22 14:05:11 -0300

    user service impl

--- src/main/java/com/jupitters/linkage/service/impl/UserServiceImpl.java
+++ src/main/java/com/jupitters/linkage/service/impl/UserServiceImpl.java
@@ -1,11 +1,21 @@
 package com.jupitters.linkage.service.impl;
 
 import com.jupitters.linkage.model.User;
+import com.jupitters.linkage.repository.UserRepository;
 import com.jupitters.linkage.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public User registerUser(User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 }
