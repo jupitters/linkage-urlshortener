983bb61b249545ab4fe9d240c5b7c55a02a6559d
jupitters <sampaiobale@gmail.com>
2026-02-22 13:51:43 -0300

    register request

---
+++ src/main/java/com/jupitters/linkage/dto/RegisterRequest.java
@@ -0,0 +1,10 @@
+package com.jupitters.linkage.dto;
+
+import java.util.Set;
+
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private Set<String> role;
+    private String password;
+}
