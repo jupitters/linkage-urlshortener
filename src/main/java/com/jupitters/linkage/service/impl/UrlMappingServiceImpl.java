bea486c68efb6124a7b6def7734ebc24bd0adb76
jupitters <sampaiobale@gmail.com>
2026-02-23 18:14:02 -0300

    url mapping service

---
+++ src/main/java/com/jupitters/linkage/service/impl/UrlMappingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.jupitters.linkage.service.impl;
+
+import com.jupitters.linkage.service.UrlMappingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlMappingServiceImpl implements UrlMappingService {
+}
