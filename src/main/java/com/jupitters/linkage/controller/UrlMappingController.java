061c135685fa9776c2979f0180d5c7e9cb4dfda1
jupitters <sampaiobale@gmail.com>
2026-02-23 18:21:48 -0300

    url mapping controller create

--- src/main/java/com/jupitters/linkage/controller/UrlMappingController.java
+++ src/main/java/com/jupitters/linkage/controller/UrlMappingController.java
@@ -1,9 +1,29 @@
 package com.jupitters.linkage.controller;
 
+import com.jupitters.linkage.model.User;
+import com.jupitters.linkage.service.UrlMappingService;
+import com.jupitters.linkage.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/urls")
 public class UrlMappingController {
+    private final UrlMappingService urlMappingService;
+    private final UserService userService;
+
+    public ResponseEntity<UrlMappingDTO> createShortUrl(@RequestBody Map<String, String> request, Principal principal){
+        String originalUrl = request.get("originalUrl");
+        User user = userService.findByUsername(principal.getName());
+        UrlMappingDto shortUrl = urlMappingService.createShortUrl(originalUrl, user);
+
+        return ResponseEntity.ok(shortUrl);
+    }
 }
