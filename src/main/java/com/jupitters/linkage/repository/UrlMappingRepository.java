4ce4961061cc2f6bb355ff44d851ed32c3fb50ce
jupitters <sampaiobale@gmail.com>
2026-02-23 18:41:54 -0300

    url mapping repo

--- src/main/java/com/jupitters/linkage/repository/UrlMappingRepository.java
+++ src/main/java/com/jupitters/linkage/repository/UrlMappingRepository.java
@@ -4,4 +4,5 @@ import com.jupitters.linkage.model.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    UrlMapping findByShortUrl(String shortUrl);
 }
--- src/main/java/com/jupitters/linkage/service/impl/UrlMappingServiceImpl.java
+++ src/main/java/com/jupitters/linkage/service/impl/UrlMappingServiceImpl.java
@@ -3,13 +3,18 @@ package com.jupitters.linkage.service.impl;
 import com.jupitters.linkage.dto.UrlMappingDTO;
 import com.jupitters.linkage.model.UrlMapping;
 import com.jupitters.linkage.model.User;
+import com.jupitters.linkage.repository.UrlMappingRepository;
 import com.jupitters.linkage.service.UrlMappingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class UrlMappingServiceImpl implements UrlMappingService {
+    private final UrlMappingRepository urlMappingRepository;
+
     @Override
     public UrlMappingDTO createShortUrl(String originalUrl, User user) {
         String shortUrl = generateShortUrl();
@@ -19,6 +24,6 @@ public class UrlMappingServiceImpl implements UrlMappingService {
         urlMapping.setUser(user);
         urlMapping.setCreatedDate(LocalDateTime.now());
 
-        return urlRepository.save(urlMapping);
+        return urlMappingRepository.save(urlMapping);
     }
 }
