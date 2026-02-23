8bb3b49decea0755a4dea65cb49f0b4f92cf85a5
jupitters <sampaiobale@gmail.com>
2026-02-23 18:42:50 -0300

    url mapping repo

--- src/main/java/com/jupitters/linkage/repository/UrlMappingRepository.java
+++ src/main/java/com/jupitters/linkage/repository/UrlMappingRepository.java
@@ -1,8 +1,12 @@
 package com.jupitters.linkage.repository;
 
 import com.jupitters.linkage.model.UrlMapping;
+import com.jupitters.linkage.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
     UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User user);
 }
