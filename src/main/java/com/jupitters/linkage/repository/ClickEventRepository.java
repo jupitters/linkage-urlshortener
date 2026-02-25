8cb3fe29e28bdd7531f9312074b897ca7911b6a5
jupitters <sampaiobale@gmail.com>
2026-02-25 11:04:58 -0300

    url mapping repository

--- src/main/java/com/jupitters/linkage/repository/ClickEventRepository.java
+++ src/main/java/com/jupitters/linkage/repository/ClickEventRepository.java
@@ -9,5 +9,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ClickEventRepository extends JpaRepository<ClickEvent, Long> {
-    List<ClickEvent> findByUrlMappingAndClickDateBetween(UrlMapping urlMapping, LocalDateTime startDate, LocalDateTime endDate);
+    List<ClickEvent> findByUrlMappingAndClickDateBetween(UrlMapping mapping, LocalDateTime startDate, LocalDateTime endDate);
+    List<ClickEvent> findByUrlMappingInAndClickDateBetween(List<UrlMapping> urlMapping, LocalDateTime startDate, LocalDateTime endDate);
 }
