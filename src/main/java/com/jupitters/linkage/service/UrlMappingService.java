a968e8d37805110ef2c9e2375bf8c606b4b81caa
jupitters <sampaiobale@gmail.com>
2026-02-25 10:54:35 -0300

    url mapping service analytics

--- src/main/java/com/jupitters/linkage/service/UrlMappingService.java
+++ src/main/java/com/jupitters/linkage/service/UrlMappingService.java
@@ -1,12 +1,16 @@
 package com.jupitters.linkage.service;
 
+import com.jupitters.linkage.dto.ClickEventDTO;
 import com.jupitters.linkage.dto.UrlMappingDTO;
 import com.jupitters.linkage.model.User;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface UrlMappingService {
     UrlMappingDTO createShortUrl(String originalUrl, User user);
 
     List<UrlMappingDTO> getUrslByUser(User user);
+
+    List<ClickEventDTO> getClickEventsByDate(String shortUrl, LocalDateTime start, LocalDateTime end);
 }
