660729f1d825e086d20be55f82f7acef26ec0a5b
jupitters <sampaiobale@gmail.com>
2026-02-26 11:00:03 -0300

    redirect controller

--- src/main/java/com/jupitters/linkage/controller/RedirectController.java
+++ src/main/java/com/jupitters/linkage/controller/RedirectController.java
@@ -5,6 +5,7 @@ import com.jupitters.linkage.service.UrlMappingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedirectController {
     private final UrlMappingService urlMappingService;
 
+    @GetMapping("/{shortUrl}")
     public ResponseEntity<Void> redirect(@PathVariable String shortUrl){
         UrlMapping urlMapping = urlMappingService.getOriginalUrl(shortUrl);
         if(urlMapping != null){
